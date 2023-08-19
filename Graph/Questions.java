5ca1ed83830f9f57f994f6534ca6e3e4e55d3a04
Priya Maurya <priyamaurya@Priyas-MacBook-Air.local>
2023-08-19 23:51:38 +0530

    grapgh dfs topo

--- Graph/Questions.java
+++ Graph/Questions.java
@@ -152,5 +152,45 @@ public class Questions {
         }
         return finalAns;
     }
+
+    //329. Longest Increasing Path in a Matrix
+
+    public int longestIncreasingPath(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] indegree=new int[n][m];
+        int[][] dir={{1,0},{0,1},{0,-1},{-1,0}};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int d=0;d<dir.length;d++){
+                    int x=i+dir[d][0];
+                    int y=j+dir[d][1];
+                    if(x>=0 && y>=0 && x<n && y<m && matrix[x][y]>matrix[i][j]){
+                        indegree[x][y]++;
+                    }}}}
+        LinkedList<Integer> que=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(indegree[i][j]==0) que.addLast(i*m+j);
+            }}
+            int level=0;
+            while(que.size()!=0){
+                int size=que.size();
+                while(size-->0){
+                    int idx=que.removeFirst();
+
+                    int r=idx/m;
+                    int c=idx%m;
+                    for(int d=0;d<dir.length;d++){
+                        int x=r+dir[d][0];
+                        int y=c+dir[d][1];
+                        if(x>=0 && y>=0 && x<n && y<m && matrix[x][y] > matrix[r][c] && --indegree[x][y] ==0){
+                            que.addLast(x*m+y);
+                    } }}
+                level++;
+            }
+        return level;  
+    }
+
     
 }
