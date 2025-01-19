21fbb48d87a70c293dc55132f3aca33b72873205
anandmaurya24 <anandmaurya294@gmail.com>
2025-01-20 00:40:59 +0530

    merge 2 sorted linked lists

---
+++ Array and String/5.longest-palindromic-substring.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (32.57%)
+ * Likes:    30188
+ * Dislikes: 1856
+ * Total Accepted:    3.6M
+ * Total Submissions: 10.1M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        
+    }
+}
+// @lc code=end
+
