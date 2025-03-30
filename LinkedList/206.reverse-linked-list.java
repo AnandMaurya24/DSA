153111c9889f6e6785fc1346f00a16de87d5e081
anandmaurya24 <anandmaurya294@gmail.com>
2025-03-30 13:28:13 +0530

    revese and middle of linked list

---
+++ LinkedList/206.reverse-linked-list.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (78.78%)
+ * Likes:    22694
+ * Dislikes: 513
+ * Total Accepted:    5.1M
+ * Total Submissions: 6.5M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev =null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode temp = curr;
+            curr = curr.next;
+            temp.next = prev;
+            prev = temp;
+        }
+        return prev;
+        
+    }
+}
+// @lc code=end
+
