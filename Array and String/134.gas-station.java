d2d16451ce557721951849a7e5ad7c3754126030
anandmaurya24 <anandmaurya294@gmail.com>
2025-04-16 23:59:32 +0530

    solution

--- Array and String/134.gas-station.java
+++ Array and String/134.gas-station.java
@@ -75,8 +75,19 @@
 // @lc code=start
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        
+        int extragas=0;
+        int deficit=0;
+        int sp=0;
+        for(int i=0;i<gas.length;i++){
+            extragas+=gas[i]-cost[i];
+            if(extragas<0){
+                    sp=i+1;
+                    deficit+=extragas;
+                    extragas=0;
+                    
+            }
+        }
+        return sp==gas.length||extragas+deficit<0?-1:sp;
     }
-}
 // @lc code=end
 
