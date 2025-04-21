b38517b35b0d3651fc6785ebb8c01281d9334344
anand <41535766+AnandMaurya24@users.noreply.github.com>
2025-04-22 01:01:21 +0530

    Create longestSubatring.java
    longest Subatring without repeating character

---
+++ Array and String/longestSubatring.java
@@ -0,0 +1,23 @@
+Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        int si=0,ei=0;
+        
+        int[] arr=new int[128];
+        
+        int n=s.length();
+        int count=0;
+        int maxLength=(int)-1e8;
+        if(s.length()<=1) return s.length();
+        while(ei<n){
+            if(arr[s.charAt(ei++)]++>0) count++;
+            
+            while(count>0)
+                if(arr[s.charAt(si++)]-->1) count--;
+            
+            maxLength=Math.max(maxLength,ei-si);
+        }
+        return maxLength;
+        
+    }
+}
